7e7b3098bf266e0c26f7926abb219318a9001283
kirillkrohmal <krohmal_kirill@mail.ru>
2020-12-10 16:04:17 +0300

    Initial commit

---
+++ src/main/java/com/restarauntvote/restarauntservice/RestarauntApplication.java
@@ -0,0 +1,13 @@
+package com.restarauntvote.restarauntservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestarauntApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestarauntApplication.class, args);
+	}
+
+}
